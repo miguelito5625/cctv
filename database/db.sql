48b232d0946d0a2b8ab4126b73be75a614f6944f
exbinario <miguel.4to.1994@gmail.com>
2019-06-21 21:07:48 -0600

    cambios menores

--- database/db.sql
+++ database/db.sql
@@ -3,14 +3,14 @@ use cctv;
 
 -- Tabla de los gabinetes --------------------------------------------------------
 
-DROP table Gabinete;
-CREATE TABLE Gabinete(
+DROP TABLE IF EXISTS Gabinete;
+CREATE TABLE IF NOT EXISTS Gabinete(
   id INT NOT NULL AUTO_INCREMENT,
   identificador varchar(50) NULL,
   ubicacion varchar(150) NULL,
   latitud DECIMAL(16, 14) NULL,
   longitud DECIMAL(16, 14) NULL,
-  estado varchar(15) NULL,
+  estado varchar(15) DEFAULT 'ACTIVO',
   updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   PRIMARY KEY(id)
@@ -24,8 +24,8 @@ FROM Gabinete;
 
 -- Tabla de las camaras --------------------------------------------------------
 
-DROP table Dispositivo;
-CREATE TABLE Dispositivo(
+DROP TABLE IF EXISTS Dispositivo;
+CREATE TABLE IF NOT EXISTS Dispositivo(
   id INT NOT NULL AUTO_INCREMENT,
   modelo varchar(25) NULL,
   n_serie varchar(25) NULL,
@@ -33,7 +33,7 @@ CREATE TABLE Dispositivo(
   id_tipoDispositivo INT NULL,
   id_marca INT NULL,
   id_gabinete int NULL,
-  estado varchar(15) NULL,
+  estado varchar(15) DEFAULT 'EN BODEGA',
   updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   PRIMARY KEY(id),
@@ -46,11 +46,14 @@ CREATE TABLE Dispositivo(
 DROP VIEW IF EXISTS view_Dispositivo;
 
 CREATE VIEW view_Dispositivo AS
-SELECT Dispositivo.id, Dispositivo.dispositivo, Dispositivo.marca, Dispositivo.modelo, Dispositivo.n_serie,
-CONCAT(latitud, ',', longitud) AS coordenadas, CONCAT(Dispositivo.updated_at, '') AS updated_at, 
-CONCAT(Dispositivo.created_at, '') AS created_at, Gabinete.identificador, Dispositivo.estado
+SELECT Dispositivo.id, tipoDispositivo.dispositivo, Marca.marca, Dispositivo.modelo, Dispositivo.n_serie,
+Gabinete.identificador as identificador_gabinete, Dispositivo.descripcion,
+CONCAT(Gabinete.latitud, ',', Gabinete.longitud) AS coordenadas, CONCAT(Dispositivo.updated_at, '') AS updated_at, 
+CONCAT(Dispositivo.created_at, '') AS created_at, Dispositivo.estado
 FROM Dispositivo
-INNER JOIN Gabinete ON Dispositivo.id_gabinete =  Gabinete.id;
+INNER JOIN Gabinete ON Dispositivo.id_gabinete =  Gabinete.id
+INNER JOIN tipoDispositivo ON Dispositivo.id_tipoDispositivo =  tipoDispositivo.id
+INNER JOIN Marca ON Dispositivo.id_marca =  Marca.id;
 
 
 -- marca
--- database/stored_procedures.sql
+++ database/stored_procedures.sql
@@ -8,12 +8,12 @@ CREATE PROCEDURE cambiar_estado_gabinete(IN id_gabinete INT)
 BEGIN
 DECLARE var_estado VARCHAR(25);
 select estado into var_estado from Gabinete where id = id_gabinete limit 1;
-IF var_estado = "Activo" 
+IF var_estado = "ACTIVO" 
 THEN
-UPDATE Gabinete SET estado = 'Inactivo' WHERE id = id_gabinete;
-ELSEIF var_estado = "Inactivo" 
+UPDATE Gabinete SET estado = 'INACTIVO' WHERE id = id_gabinete;
+ELSEIF var_estado = "INACTIVO" 
 THEN
-UPDATE Gabinete SET estado = 'Activo' WHERE id = id_gabinete;
+UPDATE Gabinete SET estado = 'ACTIVO' WHERE id = id_gabinete;
 END IF;
 select estado from Gabinete limit 1;
 END //
